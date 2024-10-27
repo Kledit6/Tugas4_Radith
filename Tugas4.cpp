4634db6babccfd7e7de0afaedf0c0d61957dc58a
Kledit6 <agusradityamp@gmail.com>
2024-10-27 23:32:20 +0800

    Add files via upload
    Tugas 4 algoritma 1C MI

---
+++ Tugas4.cpp
@@ -0,0 +1,30 @@
+
+// soal 1
+// eror berada pada bagian else cout endl bagian terakhir tidak berisi tanda titik koma " ; ".
+
+// soal 2
+// eror berada pada bagian "return 0" tidak berisi tanda titik koma " ; ".
+
+// soal 3
+// eror berada pada bagian cout << "Sum is: " endl bagian terakhir tidak berisi tanda titik koma " ; ".
+
+// soal 4 
+// error karena Kurang tanda kurung " () " di bagian printMessage untuk memanggil fungsi tersebut.
+
+// soal 5
+// error berada di Penempatan kurung kurawal " } " yang salah sebelum return 0;
+
+// soal 6
+// error berada pada bagian cout << "Sum of x and y is " endl bagian terakhir tidak berisi tanda titik koma " ; ".
+
+// soal 7
+// error berada pada bagian else return n * factorial(n - 1) tidak berisi tanda titik koma " ; ".
+
+// soal 8
+// error pada bagian return 0; tidak berisi kurung kurawal " } " pada bagian akhir.
+
+// soal 9
+// Codingan sudah benar, tidak ditemukan error.
+
+// soal 10
+// error pada Tanda " + " salah digunakan dalam deklarasi y dan temp. Dan Tanda titik koma " ; " tidak ada di akhir baris y = temp. 
